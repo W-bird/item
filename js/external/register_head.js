f099701610c53530d25fd067f5ed9dc470e3fd89
吴成宏 <2038697768@qq.com>
2017-03-20 22:36:49 +0800

    提交

---
+++ js/external/register_head.js
@@ -0,0 +1,40 @@
+$.ajax({
+	type:"get",
+	url:"../html/external/register_head.html",
+	dataType:"html",
+	async:false,
+	success:function(data){
+		$("#head").append(data);
+	}
+});
+
+$("#nav").mousemove(move);
+$("#nav").mouseout(out);
+$(".sub-nav").mousemove(move);
+$(".sub-nav").mouseout(out);
+
+function move() {
+	$("#nav").css({"textDecoration":"none"});
+	$(".nav").css({"backgroundPosition":"-136px 1px"});
+	$(".li_nav").css({
+		"backgroundColor":"#fff",
+		"borderLeft":"1px solid #ccc",
+		"borderRight":"1px solid #ccc",
+		"width":"79px",
+		"paddingLeft":"19px",
+		"height":"42px"
+	});
+	$(".sub-nav").css({"display":"block"})
+}
+
+function out() {
+	$(".nav").css({"backgroundPosition":"-121px 1px"});
+	$(".li_nav").css({
+		"backgroundColor":"#f4f4f4",
+		"border":"none",
+		"width":"80px",
+		"paddingLeft":"20px",
+		"height":"40px"
+	});
+	$(".sub-nav").css({"display":"none"})
+}
