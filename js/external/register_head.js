4ac5d3911463632e85ea1053d362f0e7ddb44df2
wuchenghong <2038697768@qq.com>
2017-03-21 21:05:22 +0800

    登录静态

--- js/external/register_head.js
+++ js/external/register_head.js
@@ -1,21 +1,18 @@
-$.ajax({
-	type:"get",
-	url:"../html/external/register_head.html",
-	dataType:"html",
-	async:false,
-	success:function(data){
-		$("#head").append(data);
+//引入外部文件到 head 标签中
+$("#head").load("../html/external/register_head.html", function() {
+	var str = window.location.href;
+	var ntr  = str.substring(33);
+	if (ntr == "log-in.html") {
+		$("#logo-p").text("用户登录")
+	} else if (ntr == "sign-in.html") {
+		$("#logo-p").text("用户注册")
 	}
 });
 
-$("#nav").mousemove(move).stop();
-$("#nav").mouseout(out).stop();
-$(".sub-nav").mousemove(move);
-$(".sub-nav").mouseout(out);
-
-
-function move() {
-	$("#nav").css({"textDecoration":"none"});
+//鼠标移入显示
+$("#head").on("mouseenter","#nav,.sub-nav", function() {
+	$("#nav").css({"color":"#0cb95f"});
+	$(".sub-nav").show();
 	$(".nav").css({"backgroundPosition":"-136px 1px"});
 	$(".li_nav").css({
 		"backgroundColor":"#fff",
@@ -25,10 +22,12 @@ function move() {
 		"paddingLeft":"19px",
 		"height":"42px"
 	});
-	$(".sub-nav").css({"display":"block"})
-}
+});
 
-function out() {
+//鼠标移出隐藏
+$("#head").on("mouseleave",".li_nav", function() {
+	$("#nav").css({"color":"#333"});
+	$(".sub-nav").hide();
 	$(".nav").css({"backgroundPosition":"-121px 1px"});
 	$(".li_nav").css({
 		"backgroundColor":"#f4f4f4",
@@ -37,5 +36,4 @@ function out() {
 		"paddingLeft":"20px",
 		"height":"40px"
 	});
-	$(".sub-nav").css({"display":"none"})
-}
+})
