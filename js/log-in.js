fb40a235cbc3e053658f47d0e8b20e306f7466fb
wuchenghong <2038697768@qq.com>
2017-03-22 16:15:37 +0800

    项目要求

--- js/log-in.js
+++ js/log-in.js
@@ -5,3 +5,14 @@ $(".ckbox").click(function() {
 		$(".ck").addClass("ck-back");
 	}
 })
+$(".log").click(function() {
+	var valname = $(".ipt-name").val();
+	var valpwd = $(".ipt-pwd").val();
+	if (valname != "" && valpwd != "") {
+		console.log('成功')
+		$(".text").hide();
+	} else{
+		console.log("失败");
+		$(".text").show();
+	}
+})
